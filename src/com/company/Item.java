2a2010d0dc89fe458937e7691406d78b11208ba4
Dperr <perryd37@students.rowan.edu>
2019-09-17 08:32:03 -0400

    Kiefer Don't kill me

---
+++ src/com/company/Item.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Item {
+    private String name;
+    private String type;
+    private int modifier;
+    private String modiType;
+    public Item(String newname, String newtype){
+        name = newname;
+        type = newtype;
+    }
+    public use(){
+
+    }
+    /*
+    public use(target){
+        target -= modifier;
+    }
+     */
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,8 @@
 package com.company;
-
+import item.java;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code herehello
+
     }
 }
